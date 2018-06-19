3511dcc811a59870bcb9474c64b7d3779a7bd5d6
Ishani <ishaniamin@gmail.com>
2018-06-19 04:19:18 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -18,13 +18,13 @@
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 def address(hash)
-  hash[:address]
+  hash["address"]
 end
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 def borough(hash)
-  hash[:borough]
+  hash["borough"]
 end
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
